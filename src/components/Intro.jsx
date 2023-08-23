af7f8fe18698f622283e212e0b6426cf33dc7dbf
rafalos99 <rafsanzaniludhi@gmail.com>
2023-08-23 15:32:14 +0100

    intro update

---
+++ src/components/Intro.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "../styles/Intro.css";
+
+
+
+
+
+const Intro = () => {
+
+
+
+    return (
+        <div id="intro">
+           
+
+            <div className="intro-container">
+                <div className="ut-dialogue">hey there!</div>
+                <span className="ut-dialogue">
+                    <span className="ut-dialogue-name intro-name">khadija</span>
+                    {" here :)"}
+                </span>
+            </div>
+        </div>
+    );
+};
+
+
+export default Intro;
+
