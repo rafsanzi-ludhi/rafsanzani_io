018e2e50ef41a3a29b4053fee6ae710edaf031e2
rafalos99 <rafsanzaniludhi@gmail.com>
2023-08-23 15:25:02 +0100

    skeleton boilerplate created

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import SidebarNav from "./components/SidebarNav";
+import Intro from "./components/Intro";
+import About from "./components/About";
+import Projects from "./components/Projects";
+// import Experience from "./components/Experience";
+import Toolkit from "./components/Toolkit";
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='App'>
+      <div id='content'>
+        <div className=' z-0'>
+          {/* <Intro></Intro> */}
+        </div>
+
+        {/* <div className='relative z-o'> */}
+          
+            {/* <About></About> */}
+            {/* <Projects></Projects> */}
+            {/* <Toolkit></Toolkit> */}
+            
+
+        {/* </div> */}
+        
+        {/* <Experience></Experience> */}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      {/* <SidebarNav></SidebarNav> */}
+      
+    </div>
+    
   )
 }
 
