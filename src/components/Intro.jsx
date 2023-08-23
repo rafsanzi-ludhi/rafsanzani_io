0e5a829dfebae672d89ba028deb16175fc16fa8e
rafalos99 <rafsanzaniludhi@gmail.com>
2023-08-23 19:10:35 +0100

    update intro.jsx and intro.css

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,17 +1,46 @@
 import React from "react";
+import "../Undertale_Stylesheet/undertale.css";
 import "../styles/Intro.css";
+import Lottie from 'react-lottie';
+import Astronaut from "../assets/astronaut.json";
+
+import Bounce from 'react-reveal/Bounce';
+import Fade from "react-reveal/Fade";
 
 
 
-
+const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: Astronaut,
+    rendererSettings: {
+        preserveAspectRatio: 'xMidYMid slice'
+    }
+};
 
 const Intro = () => {
+    const [windowWidth, setWindowWidth] = React.useState(window.innerWidth);
 
+    React.useEffect(() => {
+        const handleResize = () => setWindowWidth(window.innerWidth);
 
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
 
     return (
         <div id="intro">
-           
+            { (
+                <Bounce duration={1000}>
+                    <Lottie options={defaultOptions}
+                        
+                        className="lottie-animation" 
+                    />git pu
+                </Bounce>
+            )}
 
             <div className="intro-container">
                 <div className="ut-dialogue">hey there!</div>
@@ -26,4 +55,6 @@ const Intro = () => {
 
 
 export default Intro;
+
+
 
