71405ef6e03b8ac5b4430505b5948bfef7fa1acc
rafalos99 <rafsanzaniludhi@gmail.com>
2023-08-26 00:09:30 +0100

    added project descriptions and images

--- src/assets/index.js
+++ src/assets/index.js
@@ -29,6 +29,8 @@ import tesla from "./company/tesla.png";
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
+import calculator from "./calculator.png";
+import sketchmaze from "./sketch-maze.png";
 
 export {
   logo,
@@ -59,4 +61,6 @@ export {
   carrent,
   jobit,
   tripguide,
+  calculator,
+  sketchmaze,
 };
--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -21,9 +21,10 @@ import {
   shopify,
   carrent,
   jobit,
+  calculator,
   tripguide,
   threejs,
-  
+  sketchmaze
 
 } from "../assets";
 
@@ -117,7 +118,7 @@ const technologies = [
 ];
 
 
-// ! keep experiecne section
+// ! example experience section for use later: 
 const experiences = [
   {
     title: "React.js Developer",
@@ -173,32 +174,6 @@ const experiences = [
   },
 ];
 
-const testimonials = [
-  {
-    testimonial:
-      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-    name: "Sara Lee",
-    designation: "CFO",
-    company: "Acme Co",
-    image: "https://randomuser.me/api/portraits/women/4.jpg",
-  },
-  {
-    testimonial:
-      "I've never met a web developer who truly cares about their clients' success like Rick does.",
-    name: "Chris Brown",
-    designation: "COO",
-    company: "DEF Corp",
-    image: "https://randomuser.me/api/portraits/men/5.jpg",
-  },
-  {
-    testimonial:
-      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-    name: "Lisa Wang",
-    designation: "CTO",
-    company: "456 Enterprises",
-    image: "https://randomuser.me/api/portraits/women/6.jpg",
-  },
-];
 
 
 // ! keep projects section
@@ -225,9 +200,10 @@ const projects = [
     source_code_link: "https://github.com/",
   },
   {
-    name: "Rock Paper Scissors",
+    name: "Sketch-Maze",
     description:
-      "A simple game of rock paper scissors built using React.js.",
+      
+      "An etch-e-sketch game that allows users to draw their own fun designs.",
     tags: [
       {
         name: "react",
@@ -242,7 +218,7 @@ const projects = [
         color: "pink-text-gradient",
       },
     ],
-    image: jobit,
+    image: sketchmaze,
     source_code_link: "https://github.com/",
   },
   {
@@ -263,9 +239,9 @@ const projects = [
         color: "pink-text-gradient",
       },
     ],
-    image: tripguide,
+    image: calculator,
     source_code_link: "https://github.com/",
   },
 ];
 
-export { services, technologies, experiences, testimonials, projects };
+export { services, technologies, experiences, projects };
